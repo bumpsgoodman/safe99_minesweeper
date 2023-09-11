a14fe12f7e849f5c088007e1e4475ab0d8af8195
AzureAD\강범수 <bumpsgoodman@gmail.com>
2023-09-12 03:12:04 +0900

    Remove: 중복 파일 제거

--- minesweeper/minesweeper/source/source/safe99_common/safe_delete.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef SAFE_DELETE_H
-#define SAFE_DELETE_H
-
-#include <stdlib.h>
-
-#define SAFE_FREE(p)         { free((p)); (p) = NULL; }
-#define SAFE_RELEASE(p)      { if ((p)) { (p)->Release(); (p) = NULL; } }
-
-#endif // SAFE_DELETE_H
